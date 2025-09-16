6cff0447ffd05a830152e304c6b92ada57893679
frederic <betukumesukabamba@email.com>
2025-09-16 23:51:25 +0200

    correction local

--- src/pages/DashboardHome.vue
+++ src/pages/DashboardHome.vue
@@ -80,5 +80,8 @@ school.examensView
     }))
 )
 
+console.log(upcomingExamRows.value);
+
+
 function toFR(iso){ if(!iso) return ''; const [y,m,d]=iso.split('-'); return `${d}/${m}/${y}` }
 </script>
